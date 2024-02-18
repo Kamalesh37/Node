104d3e7f5bfe6136feba2979a5d761488d969f72
Kamalesh37 <160410501+Kamalesh37@users.noreply.github.com>
2024-02-18 20:44:31 +0530

    Update node.java

--- node.java
+++ node.java
@@ -1,70 +1,77 @@
 
 import java.util.Scanner;
-public class Main {
+
+public class NeuralNetworkApplication {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner inputScanner = new Scanner(System.in);
 
         System.out.print("Enter the number of layers in the neural network: ");
-        int numLayers = scanner.nextInt();
+        int layersCount = inputScanner.nextInt();
 
-        int[] numNodesInLayer = new int[numLayers];
-        for (int i = 0; i < numLayers; i++) {
+        int[] nodesInEachLayer = new int[layersCount];
+        for (int i = 0; i < layersCount; i++) {
             System.out.print("Enter the number of nodes in layer " + (i + 1) + ": ");
-            numNodesInLayer[i] = scanner.nextInt();
+            nodesInEachLayer[i] = inputScanner.nextInt();
         }
 
-        NeuralNetwork neuralNetwork = new NeuralNetwork(numLayers, numNodesInLayer);
+        NeuralNetworkModel neuralNetworkModel = new NeuralNetworkModel(layersCount, nodesInEachLayer);
 
         // Set edge weights
-        for (int i = 1; i < numLayers; i++) {
-            for (int j = 0; j < numNodesInLayer[i - 1]; j++) {
-                for (int k = 0; k < numNodesInLayer[i]; k++) {
+        for (int i = 1; i < layersCount; i++) {
+            for (int j = 0; j < nodesInEachLayer[i - 1]; j++) {
+                for (int k = 0; k < nodesInEachLayer[i]; k++) {
                     System.out.print("Enter weight for edge from node " + (j + 1) +
                                      " in layer " + i + " to node " + (k + 1) + " in layer " + (i + 1) + ": ");
-                    double weight = scanner.nextDouble();
-                    neuralNetwork.setEdgeWeight(i, j + 1, k + 1, weight);
+                    double edgeWeight = inputScanner.nextDouble();
+                    neuralNetworkModel.setEdgeWeight(i, j + 1, k + 1, edgeWeight);
                 }
             }
         }
 
         // Query edge weights
         System.out.print("Enter the layer of the edge: ");
-        int layer = scanner.nextInt();
+        int edgeLayer = inputScanner.nextInt();
         System.out.print("Enter the source node in the layer: ");
-        int fromNode = scanner.nextInt();
+        int fromNode = inputScanner.nextInt();
         System.out.print("Enter the destination node in the next layer: ");
-        int toNode = scanner.nextInt();
+        int toNode = inputScanner.nextInt();
 
-        double edgeWeight = neuralNetwork.getEdgeWeight(layer, fromNode, toNode);
-        System.out.println("Weight of the edge: " + edgeWeight);
+        double queriedEdgeWeight = neuralNetworkModel.getEdgeWeight(edgeLayer, fromNode, toNode);
+        System.out.println("Weight of the edge: " + queriedEdgeWeight);
 
-        scanner.close();
+        inputScanner.close();
     }
 }
 
-class NeuralNetwork {
-    private int numLayers;
-    private int[] numNodesInLayer;
-    private double[][][] weights;
+class NeuralNetworkModel {
+    private int layersCount;
+    private int[] nodesInEachLayer;
+    private double[][][] edgeWeights;
 
     // Constructor
-    public NeuralNetwork(int numLayers, int[] numNodesInLayer) {
-        this.numLayers = numLayers;
-        this.numNodesInLayer = numNodesInLayer;
-        this.weights = new double[numLayers - 1][][];
+    public NeuralNetworkModel(int layersCount, int[] nodesInEachLayer) {
+        this.layersCount = layersCount;
+        this.nodesInEachLayer = nodesInEachLayer;
+        this.edgeWeights = new double[layersCount - 1][][];
         
-        for (int i = 0; i < numLayers - 1; i++) {
-            int nodesInCurrentLayer = numNodesInLayer[i];
-            int nodesInNextLayer = numNodesInLayer[i + 1];
+        for (int i = 0; i < layersCount - 1; i++) {
+            int currentLayerNodes = nodesInEachLayer[i];
+            int nextLayerNodes = nodesInEachLayer[i + 1];
             
-            weights[i] = new double[nodesInCurrentLayer][nodesInNextLayer];
+            edgeWeights[i] = new double[currentLayerNodes][nextLayerNodes];
         }
     }
 
     // Method to set edge weight
     public void setEdgeWeight(int layer, int fromNode, int toNode, double weight) {
-        weights[layer - 1][fromNode - 1][toNode - 1] = weight;
+        edgeWeights[layer - 1][fromNode - 1][toNode - 1] = weight;
     }
+
+    // Method to get edge weight
+    public double getEdgeWeight(int layer, int fromNode, int toNode) {
+        return edgeWeights[layer - 1][fromNode - 1][toNode - 1];
+    }
+}
 
     // Method to get edge weight
     public double getEdgeWeight(int layer, int fromNode, int toNode) {
